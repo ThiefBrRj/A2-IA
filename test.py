549c2ecf269de336e80b390ee630d25aa32cdbaa
Thiago Fagundes <99626328+ThiefBrRj@users.noreply.github.com>
2023-04-27 21:37:40 -0300

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import os
 
 # Caminho para o arquivo do modelo
 modelo_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'modelo.pkl')
